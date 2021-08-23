7a5dcaedbf4c3c8f1e97f26235f4645a92744598
Karina Hutula <khutula@g.clemson.edu>
2021-08-23 09:18:00 -0400

    debug issues with index route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # import dependencies
-from Flask import Flask, jsonify
+from flask import Flask, jsonify
 
 # create app instance
 app = Flask(__name__)
@@ -7,9 +7,12 @@ app = Flask(__name__)
 # create index route
 @app.route("/")
 def home():
-    return ("The possible routes are:\none")
+    return (
+        f"Available Routes:<br/>"
+            f"one"
+        )
 
 
 # run in debug mode
-if __name__ = "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
