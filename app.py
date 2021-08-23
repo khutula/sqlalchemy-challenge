e6b25388aadfda55c2ee8d759004873a7c3245a1
Karina Hutula <khutula@g.clemson.edu>
2021-08-23 09:12:30 -0400

    create index route

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from Flask import Flask, jsonify
 # create app instance
 app = Flask(__name__)
 
+# create index route
+@app.route("/")
+def home():
+    return ("The possible routes are:\none")
+
 
 # run in debug mode
 if __name__ = "__main__":
